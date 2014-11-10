ee6e0603a4ba8317671a8f96677ed1d4d7edd6b6
maxisoft <maxisoft.eu@gmail.com>
2014-11-10 08:37:52 +0100

    CalendarHelper now use WeakReference in order to keep context reference cleanup CalendarHelper

--- app/src/main/java/planning/maxisoft/ufc/CalendarHelper.java
+++ app/src/main/java/planning/maxisoft/ufc/CalendarHelper.java
@@ -2,12 +2,14 @@ package planning.maxisoft.ufc;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.koushikdutta.ion.Ion;
 
@@ -18,6 +20,7 @@ import net.fortuna.ical4j.model.component.VEvent;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
@@ -29,21 +32,22 @@ import java.util.concurrent.TimeUnit;
 
 public class CalendarHelper {
     public static final String CALENDAR_NAME = "maxisoft_ufc_planning";
-    public static final int TIMEOUT = (int) TimeUnit.MILLISECONDS.convert(15, TimeUnit.SECONDS);
+    public static final String OWNER_ACCOUNT = "ufc planning";
+    public static final int NETWORK_TIMEOUT = (int) TimeUnit.MILLISECONDS.convert(15, TimeUnit.SECONDS);
     private static final Map<Context, CalendarHelper> instances = new WeakHashMap<>();
 
-    private final Context context;
-    private long calId = -1;
+    private final WeakReference<Context> context;
+    private long calendarId = -1;
     private volatile Future<InputStream> lastDownloadTask;
     private volatile Future lastTask;
     private ExecutorService executor;
 
-    private CalendarHelper(@NonNull Context context) {
-        this.context = context;
+    private CalendarHelper(Context context) {
+        this.context = new WeakReference<>(context);
         executor = Executors.newSingleThreadExecutor();
     }
 
-    public static synchronized CalendarHelper getInstance(Context context) {
+    public static synchronized CalendarHelper getInstance(@NonNull Context context) {
         CalendarHelper instance = instances.get(context);
         if (instance == null) {
             instance = new CalendarHelper(context);
@@ -59,39 +63,42 @@ public class CalendarHelper {
                 .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL).build();
     }
 
+    private Uri createSyncAdapterUri() {
+        return asSyncAdapter(CalendarContract.Calendars.CONTENT_URI,
+                CalendarHelper.class.getPackage().getName());
+    }
+
     private long createDefaultCalendar() {
         ContentValues values = new ContentValues();
         values.put(CalendarContract.Calendars.NAME, CALENDAR_NAME);
-        values.put(CalendarContract.Calendars.CALENDAR_DISPLAY_NAME, "planning des cours");
+        values.put(CalendarContract.Calendars.CALENDAR_DISPLAY_NAME, R.string.calendar_display_name);
         values.put(CalendarContract.Calendars.VISIBLE, 1);
         values.put(CalendarContract.Calendars.ACCOUNT_NAME, MainActivity.class.getPackage().getName());
         values.put(CalendarContract.Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
-        values.put(CalendarContract.Calendars.OWNER_ACCOUNT, "ufc planning");
+        values.put(CalendarContract.Calendars.OWNER_ACCOUNT, OWNER_ACCOUNT);
         values.put(CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL, CalendarContract.Calendars.CAL_ACCESS_OWNER);
         values.put(CalendarContract.Calendars.SYNC_EVENTS, 1);
-        Uri newCal = context.getContentResolver().insert(asSyncAdapter(CalendarContract.Calendars.CONTENT_URI,
-                        MainActivity.class.getPackage().getName()),
-                values);
+        Uri newCal = getContext().getContentResolver().insert(createSyncAdapterUri(), values);
         return Long.parseLong(newCal.getLastPathSegment());
     }
 
-    public long getCalendarsId() {
-        if (calId == -1) {
+    public long getCalendarId() {
+        if (calendarId == -1) {
             Cursor cur = null;
             try {
-                cur = context.getContentResolver().query(CalendarContract.Calendars.CONTENT_URI,
+                cur = getContext().getContentResolver().query(CalendarContract.Calendars.CONTENT_URI,
                         new String[]{CalendarContract.Calendars._ID},
                         "(" + CalendarContract.Calendars.NAME + " = ?)",
                         new String[]{CALENDAR_NAME},
                         null);
-                calId = cur.moveToNext() ? cur.getLong(0) : createDefaultCalendar();
+                calendarId = cur.moveToNext() ? cur.getLong(0) : createDefaultCalendar();
             } finally {
                 if (cur != null) {
                     cur.close();
                 }
             }
         }
-        return calId;
+        return calendarId;
     }
 
     public Future downloadCalendar() {
@@ -107,25 +114,26 @@ public class CalendarHelper {
     }
 
     public synchronized void updateCalendar() {
-        long id = getCalendarsId();
+        long id = getCalendarId();
         ContentValues values = new ContentValues();
         values.put(CalendarContract.Calendars.NAME, CALENDAR_NAME);
         values.put(CalendarContract.Calendars.VISIBLE, 1);
         values.put(CalendarContract.Calendars.ACCOUNT_NAME, MainActivity.class.getPackage().getName());
         values.put(CalendarContract.Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
-        values.put(
-                CalendarContract.Calendars.CALENDAR_TIME_ZONE,
-                TimeZone.getDefault().getDisplayName());
-        int color = PreferenceManager.getDefaultSharedPreferences(context)
-                .getInt(context.getString(R.string.pref_calendar_color_key), Color.BLUE);
-        values.put(
-                CalendarContract.Calendars.CALENDAR_COLOR,
-                color);
+        values.put(CalendarContract.Calendars.CALENDAR_DISPLAY_NAME, R.string.calendar_display_name);
+        values.put(CalendarContract.Calendars.CALENDAR_TIME_ZONE, TimeZone.getDefault().getDisplayName());
+        int color = getDefaultSharedPreferences().getInt(getContext().getString(R.string.pref_calendar_color_key), Color.BLUE);
+        values.put(CalendarContract.Calendars.CALENDAR_COLOR, color);
         values.put(CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL, CalendarContract.Calendars.CAL_ACCESS_READ);
         values.put(CalendarContract.Calendars.SYNC_EVENTS, 1);
-        context.getContentResolver().update(asSyncAdapter(CalendarContract.Calendars.CONTENT_URI,
-                        MainActivity.class.getPackage().getName()),
-                values, "(" + CalendarContract.Calendars._ID + " = ?)", new String[]{String.valueOf(id)});
+        getContext().getContentResolver().update(createSyncAdapterUri(),
+                values,
+                "(" + CalendarContract.Calendars._ID + " = ?)",
+                new String[]{String.valueOf(id)});
+    }
+
+    private SharedPreferences getDefaultSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(getContext());
     }
 
     @SuppressWarnings("unchecked")
@@ -136,9 +144,9 @@ public class CalendarHelper {
     }
 
     private Future<InputStream> downloadAndSetCalendar() {
-        lastDownloadTask = Ion.with(context)
+        lastDownloadTask = Ion.with(getContext())
                 .load("https://sedna.univ-fcomte.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=8241fc38732002141e9729a804a2f84be0fa50826f0818af2370d544632bbb83906f45af276f59ae8fac93f781e861524e07c487e860b173d7c57dbfb39052a3c2973627c2eb073ba38388197b1a14238d3f4109b6629391")
-                .setTimeout(TIMEOUT)
+                .setTimeout(NETWORK_TIMEOUT)
                 .asInputStream()
                 .setCallback((e, result) -> {
                     if (result != null) {
@@ -148,27 +156,28 @@ public class CalendarHelper {
                             try {
                                 calendar = builder.build(result);
                             } catch (IOException | ParserException e1) {
-                                e1.printStackTrace();
+                                Log.e(CalendarHelper.class.getSimpleName(), "can't parse calendar", e1);
                                 return;
                             }
                             clearEvents();
                             updateCalendar();
                             addCalendar(calendar);
                         });
-
+                    } else if (e != null) {
+                        Log.e(CalendarHelper.class.getSimpleName(), "can't download calendar", e);
                     }
                 });
         return lastDownloadTask;
     }
 
-    private void addEvents(@NonNull List<VEvent> event) {
-        ContentValues values[] = new ContentValues[event.size()];
+    private void addEvents(List<VEvent> events) {
+        ContentValues values[] = new ContentValues[events.size()];
         int i = 0;
-        for (VEvent vEvent : event) {
-            values[i] = createEventContentValues(vEvent);
+        for (VEvent event : events) {
+            values[i] = createEventContentValues(event);
             i += 1;
         }
-        context.getContentResolver().bulkInsert(asSyncAdapter(CalendarContract.Events.CONTENT_URI, MainActivity.class.getPackage().getName()), values);
+        getContext().getContentResolver().bulkInsert(createSyncAdapterUri(), values);
     }
 
     private ContentValues createEventContentValues(VEvent event) {
@@ -177,7 +186,7 @@ public class CalendarHelper {
         values.put(CalendarContract.Events.DTEND, event.getEndDate().getDate().getTime());
         values.put(CalendarContract.Events.TITLE, event.getSummary().getValue());
         values.put(CalendarContract.Events.EVENT_LOCATION, event.getLocation().getValue());
-        values.put(CalendarContract.Events.CALENDAR_ID, getCalendarsId());
+        values.put(CalendarContract.Events.CALENDAR_ID, getCalendarId());
         values.put(CalendarContract.Events.DESCRIPTION, event.getDescription().getValue());
         values.put(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PUBLIC);
         values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getDisplayName());
@@ -185,9 +194,9 @@ public class CalendarHelper {
     }
 
     private void clearEvents() {
-        context.getContentResolver().delete(asSyncAdapter(CalendarContract.Events.CONTENT_URI, MainActivity.class.getPackage().getName()),
+        getContext().getContentResolver().delete(createSyncAdapterUri(),
                 "(" + CalendarContract.Events.CALENDAR_ID + " = ?)",
-                new String[]{String.valueOf(getCalendarsId())});
+                new String[]{String.valueOf(getCalendarId())});
     }
 
     public void cancelTasks() {
@@ -195,6 +204,10 @@ public class CalendarHelper {
         if (lastTask != null) {
             lastTask.cancel(true);
         }
+    }
+
+    private Context getContext() {
+        return context.get();
     }
 
     public Future getLastTask() {
